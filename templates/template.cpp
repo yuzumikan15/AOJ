dbac7d2691271e3b959a70901e5adaf8bf075755
Yuki Ishii <yuzu.or.orange@gmail.com>
2015-04-18 04:43:24 +0900

    add 10000

---
+++ templates/template.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <math.h>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+using namespace std;
+
+#define rep(i,n) for(int i=0;i<n;i++)
+// 初期化！
+
+void solve() {
+
+
+}
+
+int main() {
+
+
+  return 0;
+}
