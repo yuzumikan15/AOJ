6b3c565e2acabaffb02834d2b99b1092ffd70d1a
Yuki Ishii <yuzu.or.orange@gmail.com>
2015-04-18 04:55:44 +0900

    add 10001

---
+++ templates/template.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args){
+		// Scanner for input
+		Scanner sc = new Scanner(System.in);
+		// input integer
+		int a = sc.nextInt();		
+		// input string
+		String s = sc.next();
+		// output
+		System.out.println(a + s);
+	}
+}
